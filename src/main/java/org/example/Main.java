41054e84598c9c3263c9f687026eb7c898ef7d50
Rinzlertron456 <18r01a04b3@cmritonline.ac.in>
2024-08-01 20:13:14 +0530

    push

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        int res = list.stream().map(n -> n * 2).filter(n -> (n % 2 == 0)).reduce(0, (c, e) -> c + e);
+        System.out.println(res);
     }
 }
