a9324282552f6b0945d7ab1576304988c1020574
Rinzlertron456 <18r01a04b3@cmritonline.ac.in>
2024-08-06 19:21:26 +0530

    push

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
         int res = list.stream().map(n -> n * 2).filter(n -> (n % 2 == 0)).reduce(0, (c, e) -> c + e);
-        System.out.println(res);
+        System.out.println(res+"FU");
     }
 }
